34143813cd2bdcf6aa563a2cd3edb587e83d74bf
tomsx <wantom@gmx.net>
2018-02-27 15:29:36 +0100

    Add files via upload

---
+++ nuScroll_mousewheel.js
@@ -0,0 +1,19 @@
+function nuScrollBindMouseWheel(n) {
+    $('.nuScroll').bind("mousewheel", function (event, delta) {
+        $(this).focus();
+        var e = jQuery.Event("keydown");
+        if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+            e.keyCode = 38; // up arrow  
+        } else {
+            e.keyCode = 40; // down arrow  
+        }
+        $(this).trigger(e);
+        return false;
+    });
+}
+
+function nuOnLoad() {
+    if (nuFormType() == 'edit') {
+        nuScrollBindMouseWheel(".nuScroll");
+    }
+}
