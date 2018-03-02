7f3ad14a7bc0666248428662f2abe9b8cdb21a23
tomsx <wantom@gmx.net>
2018-03-02 13:10:39 +0100

    Create zebraStripeBrowseTable.js

---
+++ zebraStripeBrowseTable.js
@@ -0,0 +1,31 @@
+function zebraStripeBrowseTable(color1, color2) {
+
+    $('[data-nu-column="0"]').each(function (index) {
+        var cellId = $(this).attr('id');
+        var rowNum = String(cellId).split('_')[1];
+
+        var bgColor = color1;
+        if (isEven(Number(rowNum))) {
+            bgColor = color2;
+        }
+        $("div[id^='nucell_" + rowNum).css('background-color', bgColor);
+    });
+}
+
+function isEven(n) {
+    return n % 2 == 0;
+}
+
+
+function nuOnLoad() {
+
+    if (nuFormType() == 'browse') {
+        zebraStripeBrowseTable('#f2f2f2', 'white'); // #f2f6f7
+    }
+}
+
+<style>
+.nuBrowseTitle {
+  background-color: #e8eded;
+}
+</style>
